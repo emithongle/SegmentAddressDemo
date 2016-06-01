0d2a303b37200c2656b57ef2e56fd0602f92d433
ThongLe <lpthong90@gmail.com>
2016-06-01 13:07:50 +0700

    add real segment time from server

--- logs/loger.py
+++ logs/loger.py
@@ -4,20 +4,28 @@ from time import strftime, localtime
 from address_segmentation.segment_api import segment_api_v1_1
 from config import folders, files
 from db.store import loadXLSX, saveXLSX
+from datetime import datetime 
 
 def saveLog(log, filePath=folders['log'] + '/' + files['log']['log']):
     try:
         data = loadXLSX(filePath)
+
+        startTime = datetime.now()
+        apiResults = str(segment_api_v1_1(log['ocrText']))
+        endTime = datetime.now()
+
+        realSegmentTime = (endTime - startTime).seconds * 1000 + (endTime - startTime).microseconds / 1000
 
         data.append([
             strftime("%a, %d %b %Y %H:%M:%S", localtime()),
             log['uploadTime'],
             log['ocrTime'],
             log['segmentTime'],
+            realSegmentTime,
             log['imageURL'],
             log['ocrText'],
             log['idResult'],
-            str(segment_api_v1_1(log['ocrText'])),
+            apiResults,
             log['name'],
             log['address'],
             log['phone']
